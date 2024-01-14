98078211a6f18cec107cb9da79080ce0c10e2982
iarigby <marvinzem@gmail.com>
2024-01-14 15:35:53 +0100

    add decorators for accessing collections

--- backend/nosql/db.py
+++ backend/nosql/db.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 from contextlib import contextmanager
 
+import pymongo.collection
 from bson import ObjectId
 from pymongo.mongo_client import MongoClient
 
@@ -37,15 +38,15 @@ class MongoDatabase(backend.database.Database):
         client.drop_database('imse')
 
     def get_user(self, user_id: str) -> schemas.User:
-        user = self.client.imse.users.find_one({'_id': user_id})
+        user = self.users.find_one({'_id': user_id})
         return schemas.User.model_validate(user)
 
     def get_users(self) -> list[schemas.User]:
-        users = self.client.imse.users.find()
+        users = self.users.find()
         return [schemas.User.model_validate(user) for user in users]
 
     def add_user(self, user: schemas.NewUser) -> schemas.User:
-        user_id = self.client.imse.users.insert_one(user.model_dump(by_alias=True)).inserted_id
+        user_id = self.users.insert_one(user.model_dump(by_alias=True)).inserted_id
         return schemas.User.model_validate(self.get_user(user_id))
 
     def add_artist(self, artist: schemas.NewArtist):
@@ -58,7 +59,7 @@ class MongoDatabase(backend.database.Database):
         pass
 
     def get_events(self):
-        return [schemas.Event.model_validate(event) for event in self.client.imse.events.find()]
+        return [schemas.Event.model_validate(event) for event in self.events.find()]
 
     def get_top_users_for_venue(self, venue_id) -> list[schemas.VenueReport]:
         pass
@@ -67,14 +68,14 @@ class MongoDatabase(backend.database.Database):
         pass
 
     def add_ticket(self, ticket: schemas.NewTicket) -> schemas.Ticket:
-        self.client.imse.events.update_one({
+        self.events.update_one({
             '_id': to_object_id(ticket.event_id)},
             {'$push': {'tickets': ticket.model_dump(by_alias=True)}
              })
         return self.get_ticket(ticket.user_id, ticket.event_id)
 
     def get_ticket(self, user_id: str, event_id: str) -> schemas.Ticket:
-        event = self.client.imse.events.find_one(
+        event = self.events.find_one(
             {'_id': to_object_id(event_id), 'tickets.user_id': user_id})
         ticket = next(ticket for ticket in event["tickets"] if ticket["user_id"] == user_id)
         return schemas.Ticket.model_validate(ticket)
@@ -86,7 +87,7 @@ class MongoDatabase(backend.database.Database):
         pass
 
     def get_event(self, event_id: str):
-        event = self.client.imse.events.find_one({'_id': to_object_id(event_id)})
+        event = self.events.find_one({'_id': to_object_id(event_id)})
         return schemas.Event.model_validate(event)
 
     def decrease_user_balance(self, user_id: str, amount: int):
@@ -99,28 +100,44 @@ class MongoDatabase(backend.database.Database):
         pass
 
     def add_event(self, event: schemas.NewEvent):
-        event_id = self.client.imse.events.insert_one(event.model_dump(by_alias=True)).inserted_id
+        event_id = self.events.insert_one(event.model_dump(by_alias=True)).inserted_id
         return schemas.Event.model_validate(self.get_event(event_id))
 
     def add_venue(self, venue: schemas.NewVenue):
-        venue_id = self.client.imse.venues.insert_one(venue.model_dump(by_alias=True)).inserted_id
+        venue_id = self.venues.insert_one(venue.model_dump(by_alias=True)).inserted_id
         return schemas.Venue.model_validate(self.get_venue(venue_id))
 
     def get_venue(self, venue_id: str):
-        venue = self.client.imse.venues.find_one({'_id': venue_id})
+        venue = self.venues.find_one({'_id': venue_id})
         return schemas.Venue.model_validate(venue)
 
     def get_venues(self):
-        return [schemas.Venue.model_validate(venue) for venue in self.client.imse.venues.find()]
+        return [schemas.Venue.model_validate(venue) for venue in self.venues.find()]
 
     def add_users(self, users: list[schemas.User]):
-        self.client.imse.users.insert_many([user.model_dump(by_alias=True) for user in users])
+        self.users.insert_many([user.model_dump(by_alias=True) for user in users])
 
     def add_venues(self, venues: list[schemas.Venue]):
-        self.client.imse.venues.insert_many([venue.model_dump(by_alias=True) for venue in venues])
+        self.venues.insert_many([venue.model_dump(by_alias=True) for venue in venues])
 
     def add_events(self, events: list[schemas.Event]):
-        self.client.imse.events.insert_many([event.model_dump(by_alias=True) for event in events])
+        self.events.insert_many([event.model_dump(by_alias=True) for event in events])
 
     def add_tickets(self, tickets: list[schemas.Ticket]):
         pass
+
+    @property
+    def events(self) -> pymongo.collection.Collection:
+        return self.client.imse.events
+
+    @property
+    def users(self) -> pymongo.collection.Collection:
+        return self.client.imse.users
+
+    @property
+    def artists(self) -> pymongo.collection.Collection:
+        return self.client.imse.artists
+
+    @property
+    def venues(self) -> pymongo.collection.Collection:
+        return self.client.imse.venues
