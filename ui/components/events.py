6baed133f99d6546095c88b44086d211779ea1d9
iarigby <marvinzem@gmail.com>
2024-01-14 21:41:17 +0100

    have a fixed thumbnail for each event

--- ui/components/events.py
+++ ui/components/events.py
@@ -1,15 +1,16 @@
-import random
 import os
 import streamlit as st
 
 from backend import schemas
 
 
+images = os.listdir("./media/images")
+
+
 def display_event(event: schemas.Event, venue: schemas.Venue):
-    image = random.choice(os.listdir("./media/images"))
-    st.image("./media/images/" + image)
+    something_like_a_hash = int(''.join([s for s in str(event.id) if s.isdigit()][0:5])) % len(images)
+    st.image("./media/images/" + images[something_like_a_hash])
     st.write(event.date.date())
     st.write(event.name)
     st.write("location: " + venue.name + " in " + venue.city)
     st.write("€" + str(event.price))
-
